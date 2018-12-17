a945003f37ecf94d225b6f6168bdc3dc677036c2
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2018-12-17 19:44:02 +0100

    Update augmentation.py

--- augmentation.py
+++ augmentation.py
@@ -25,12 +25,8 @@ import configuration as config
 
 def augmentation_function(images, labels):
     '''
-    :param images: A numpy array of shape [minibatch, X, Y, (Z), nchannels]
-    :param labels: A numpy array containing a corresponding label mask
-    :param do_rotations: Rotate the input images by a random angle between -15 and 15 degrees.
-    :param do_scaleaug: Do scale augmentation by sampling one length of a square, then cropping and upsampling the image
-                        back to the original size. 
-    :param do_fliplr: Perform random flips with a 50% chance in the left right direction.     
+    :param images: A numpy array of shape [batch, X, Y], normalized between 0-1
+    :param labels: A numpy array containing a corresponding label mask     
     ''' 
     
     # Define in configuration.py which operations to perform
@@ -72,48 +68,72 @@ def augmentation_function(images, labels):
                 if coin_flip < prob :
                     angles = config.rg
                     random_angle = np.random.uniform(angles[0], angles[1])
-                    img = image_utils.rotate_image(img, random_angle)
-                    lbl = image_utils.rotate_image(lbl, random_angle, interp=cv2.INTER_NEAREST)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = image_utils.rotate_image(img, random_angle)
+                    lbll = image_utils.rotate_image(lbl, random_angle, interp=cv2.INTER_NEAREST)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
 
             # ROTATE 90°
             if do_rotations_90:
                 coin_flip = np.random.uniform(low=0.0, high=1.0)
                 if coin_flip < prob :
                     angle = 90
-                    img = image_utils.rotate_image(img, angle)
-                    lbl = image_utils.rotate_image(lbl, angle, interp=cv2.INTER_NEAREST)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = image_utils.rotate_image(img, angle)
+                    lbll = image_utils.rotate_image(lbl, angle, interp=cv2.INTER_NEAREST)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
         
             # ROTATE 180°
             if do_rotations_180:
                 coin_flip = np.random.uniform(low=0.0, high=1.0)
                 if coin_flip < prob :
                     angle = 180
-                    img = image_utils.rotate_image(img, angle)
-                    lbl = image_utils.rotate_image(lbl, angle, interp=cv2.INTER_NEAREST)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = image_utils.rotate_image(img, angle)
+                    lbll = image_utils.rotate_image(lbl, angle, interp=cv2.INTER_NEAREST)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
            
             # ROTATE 270°
             if do_rotations_270:
                 coin_flip = np.random.uniform(low=0.0, high=1.0)
                 if coin_flip < prob :
                     angle = 270
-                    img = image_utils.rotate_image(img, angle)
-                    lbl = image_utils.rotate_image(lbl, angle, interp=cv2.INTER_NEAREST)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = image_utils.rotate_image(img, angle)
+                    lbll = image_utils.rotate_image(lbl, angle, interp=cv2.INTER_NEAREST)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
           
             # ROTATE (generic angle with reshape)
             if do_rotation_reshape:
                 coin_flip = np.random.uniform(low=0.0, high=1.0)
                 if coin_flip < prob :
                     angle = config.angle
-                    img = scipy.ndimage.rotate(img,angle)
-                    lbl = scipy.ndimage.rotate(lbl,angle)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = scipy.ndimage.rotate(img,angle)
+                    lbll = scipy.ndimage.rotate(lbl,angle)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
             
             # ROTATE (generic angle)
             if do_rotation:
                 coin_flip = np.random.uniform(low=0.0, high=1.0)
                 if coin_flip < prob :
                     angle = config.angle
-                    img = scipy.ndimage.rotate(img,angle,reshape=False)
-                    lbl = scipy.ndimage.rotate(lbl,angle,reshape=False)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = scipy.ndimage.rotate(img,angle,reshape=False)
+                    lbll = scipy.ndimage.rotate(lbl,angle,reshape=False)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
             
             # RANDOM CROP SCALE
             if crop:
@@ -121,22 +141,34 @@ def augmentation_function(images, labels):
                 if coin_flip < prob :
                     augmenters = [iaa.Crop(px=config.offset)]
                     seq = iaa.Sequential(augmenters, random_order=True)
-                    img = seq.augment_image(img)
-                    lbl = seq.augment_image(lbl)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = seq.augment_image(img)
+                    lbll = seq.augment_image(lbl)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
 
             # RANDOM FLIP Lelf/Right
             if do_fliplr:
                 coin_flip = np.random.uniform(low=0.0, high=1.0)
                 if coin_flip < prob :
-                    img = np.fliplr(img)
-                    lbl = np.fliplr(lbl)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = np.fliplr(img)
+                    lbll = np.fliplr(lbl)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
                 
             # RANDOM FLIP  up/down
             if do_flipud:
                 coin_flip = np.random.uniform(low=0.0, high=1.0)
                 if coin_flip < prob :
-                    img = np.flipud(img)
-                    lbl = np.flipud(lbl)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = np.flipud(img)
+                    lbll = np.flipud(lbl)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
                 
             # RANDOM CONTRAST
             # Random change the passed image contrast
@@ -153,6 +185,7 @@ def augmentation_function(images, labels):
                 if coin_flip < prob :
                     factor = np.random.uniform(config.min_factor, config.max_factor)
                     img = ImageEnhance.Contrast(img).enhance(factor)
+                    # write the code (img should not be an array)
             
             # BLURRING 
             # only on image not label
@@ -161,19 +194,27 @@ def augmentation_function(images, labels):
                 if coin_flip < prob :
                     inter = config.sigma
                     sigma = np.random.uniform(inter[0],inter[1])
-                    img = scipy.ndimage.gaussian_filter(img, sigma)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = scipy.ndimage.gaussian_filter(img, sigma)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
            
             # SaltAndPepper (image should be normalized between 0 and 1)
             if SaltAndPepper:
                 coin_flip = np.random.uniform(low=0, high=1.0)
                 if coin_flip < prob :
                     dens = config.density
+                    imgg = img.copy()
+                    lbll = lbl.copy()
                     if (dens>1.00) | (dens<0.00):
                         dens = 0.05
                     coords = [np.random.randint(0, d - 1, int(np.ceil(dens * img.size * 0.5))) for d in img.shape]
-                    img[coords] = 1
+                    imgg[coords] = 1
                     coords = [np.random.randint(0, d - 1, int(np.ceil(dens * img.size * 0.5))) for d in img.shape]
-                    img[coords] = 0
+                    imgg[coords] = 0
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
             
             # Multiply
             if Multiply:
@@ -181,7 +222,11 @@ def augmentation_function(images, labels):
                 if coin_flip < prob :
                     augmenters = [iaa.Multiply(config.m)]
                     seq = iaa.Sequential(augmenters, random_order=True)
-                    img = seq.augment_image(img)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = seq.augment_image(img)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
             
             # ElasticTransformation
             '''
@@ -197,20 +242,25 @@ def augmentation_function(images, labels):
                 if coin_flip < prob :
                     augmenters = [iaa.ElasticTransformation(alpha=config.alpha, sigma=config.sigma)]
                     seq = iaa.Sequential(augmenters, random_order=True)
-                    img = seq.augment_image(img)
-                    lbl = seq.augment_image(lbl)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = seq.augment_image(img)
+                    lbll = seq.augment_image(lbl)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
             
             if Pad:
                 coin_flip = np.random.uniform(low=0, high=1.0)
                 if coin_flip < prob :
                     augmenters = [iaa.Pad(px=config.offset2)]
                     seq = iaa.Sequential(augmenters, random_order=True)
-                    img = seq.augment_image(img)
-                    lbl = seq.augment_image(lbl)
+                    imgg = img.copy()
+                    lbll = lbl.copy()
+                    imgg = seq.augment_image(img)
+                    lbll = seq.augment_image(lbl)
+                    new_images.append(imgg[...])
+                    new_labels.append(lbll[...])
             
-            
-            new_images.append(img[...])
-            new_labels.append(lbl[...])
 
         sampled_image_batch = np.asarray(new_images)
         sampled_label_batch = np.asarray(new_labels)
@@ -218,4 +268,4 @@ def augmentation_function(images, labels):
         return sampled_image_batch, sampled_label_batch
     
     else:
-        logging.warning('Probability must be in range 0.0/1.0!!')
+        logging.warning('Probability must be in range [0.0,1.0]!!')
