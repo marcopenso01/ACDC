24adff185caff071ab1ff7a971b893b50d801fde
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2019-02-21 13:15:35 +0100

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -25,7 +25,7 @@ learning_rate = 0.01   #unet: 0.01    enet: 0.0005
 optimizer_handle = tf.train.AdamOptimizer     #(beta1 = 0.9, beta2 = 0.999, epsilon=1e-08)
 schedule_lr = False
 warmup_training = True
-weight_decay = 0.00000   # enet: 0.0004
+weight_decay = 0.00000   # enet: 0.0002
 momentum = None
 # loss can be 'weighted_crossentropy'/'crossentropy'/'dice'/'dice_onlyfg'/'crossentropy_and_dice (alfa,beta)'
 loss_type = 'weighted_crossentropy'
