e667bdc7401be4641396a02fc835d3c442b08f7b
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2019-03-04 20:23:41 +0100

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -21,7 +21,7 @@ train_on_all_data = False
 
 # Training settings
 batch_size = 10
-learning_rate = 0.0005   #unet: 0.01    enet: 0.0005
+learning_rate = 0.01   #unet: 0.01    enet: 0.0005
 optimizer_handle = tf.train.AdamOptimizer     #(beta1 = 0.9, beta2 = 0.999, epsilon=1e-08)
 schedule_lr = False
 warmup_training = True
