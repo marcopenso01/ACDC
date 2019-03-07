aaecb0d47f24769d07b8c5b1c0fd077b702efb10
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2019-03-07 15:55:52 +0100

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -12,10 +12,10 @@ model_handle = model_structure.unet2D
 weight_init = 'he_normal'    # xavier_uniform/ xavier_normal/ he_normal /he_uniform /caffe_uniform/ simple/ bilinear
 
 # Model settings Enet
-xa_un = tensorflow.contrib.layers.xavier_initializer(uniform=True)
-xa_no = tensorflow.contrib.layers.xavier_initializer(uniform=False)
-he_no = tensorflow.contrib.layers.variance_scaling_initializer(uniform=False)
-he_un = tensorflow.contrib.layers.variance_scaling_initializer(uniform=True)
+iniz = tensorflow.contrib.layers.xavier_initializer(uniform=True)  #xavier_uniform
+# iniz = tensorflow.contrib.layers.xavier_initializer(uniform=False)  #xavier_normal
+# iniz = tensorflow.contrib.layers.variance_scaling_initializer(uniform=False)  #he_normal
+# iniz = tensorflow.contrib.layers.variance_scaling_initializer(uniform=True)  #he_uniform
 
 # Data settings
 data_mode = '2D'  # 2D or 3D
