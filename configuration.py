867eba022af0050384379366d95f30fa8da09efb
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2019-03-27 11:26:34 +0100

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -32,7 +32,7 @@ train_on_all_data = False
 
 # Training settings
 batch_size = 4      #4   #5   #8
-learning_rate = 0.001   #unet: 0.01    enet: 0.0005
+learning_rate = 0.0005   #unet: 0.01    enet: 0.0005
 exponential_decay = False     #True Enet
 optimizer_handle = tf.train.AdamOptimizer     #(beta1 = 0.9, beta2 = 0.999, epsilon=1e-08)
 schedule_lr = False    #decrease 10 times the LR when loss gradient lower than threshold
