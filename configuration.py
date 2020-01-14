006bb7375a680c9ed62ebdd50106a8b574e23aee
marcopenso01 <45823432+marcopenso01@users.noreply.github.com>
2020-01-14 11:38:14 +0100

    Update configuration.py

--- configuration.py
+++ configuration.py
@@ -33,7 +33,7 @@ split_test_train = True   #divide data in train (80%) and test (20%)
 train_on_all_data = False 
 
 # Training settings
-batch_size = 4      #4   #5   #8
+batch_size = 5      #4   #5   #8
 learning_rate = 0.01   #unet: 0.01    enet: 0.0005
 exponential_decay = False     #True Enet
 optimizer_handle = tf.compat.v1.train.AdamOptimizer     #(beta1 = 0.9, beta2 = 0.999, epsilon=1e-08)   #tf.train.AdamOptimizer
